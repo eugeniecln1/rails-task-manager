58164d1f3024a8b3933c05f36193004eb29ba779
Eugenie Colonna d Istria <eugeniecolonnadistria@gmail.com>
2019-05-17 15:14:56 +0200

    routes controller and views done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,11 @@
 class TasksController < ApplicationController
+  before_action :set_tasks, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def new
@@ -18,16 +19,23 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to task_path(@task)
   end
 
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
+
+  def set_tasks
+    @task = Task.find(params[:id])
+  end
 
   def task_params
     params.require(:task).permit(:title, :details)
